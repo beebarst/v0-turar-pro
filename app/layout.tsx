2aa8127af9d1b9b4fb6b64f2776813d7eddf936a
beebarst <turarbibars@gmail.com>
2026-07-15 14:47:44 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,18 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${inter.variable} bg-background dark`}>
       <head>
-        {/* META PIXEL: ТВОЙ_ID_ПИКСЕЛЯ_ЗДЕСЬ */}
-      </head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-V2R4YLB3ZE" />
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-V2R4YLB3ZE');
+      `,
+    }}
+  />
+</head>
       <body className="font-sans antialiased bg-transparent text-white overflow-x-hidden">
         {children}
         {process.env.NODE_ENV === "production" && <Analytics />}
